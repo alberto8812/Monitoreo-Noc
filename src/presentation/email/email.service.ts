0c50470158e8095e846e0d04b90a97ef2bd9b72f
alberto8812 <101751073+alberto8812@users.noreply.github.com>
2024-04-01 12:04:19 -0500

    create uase case for email

--- src/presentation/email/email.service.ts
+++ src/presentation/email/email.service.ts
@@ -1,11 +1,18 @@
 import nodemailer from "nodemailer";
 import { envs } from "../../config/plugins/envs.plugin";
+import { LogEntity, LogSeveritylevel } from "../../domain/entities/log.entities";
 
 interface SendMailOptions {
-  to: string;
+  to: string | string[];
   subject: string;
   htmlbody: string;
   //todo attachment
+  attachement?:Attachement[]
+}
+
+interface Attachement {
+    filename: string,
+    path:string// stream this file
 }
 
 export class EmailService {
@@ -17,19 +24,50 @@ export class EmailService {
     },
   });
 
+  constructor(
+  ){
+
+  }
+
   async sendEmail(options: SendMailOptions): Promise<boolean> {
-    const { to, subject, htmlbody } = options;
+    const { to, subject, htmlbody,attachement=[] } = options;
 
     try {
         const sentInformation=await this.transporter.sendMail({
             to:to,
             subject:subject,
-            html:htmlbody
+            html:htmlbody,
+            attachments:attachement
+        });
+        const log=new LogEntity({
+          level: LogSeveritylevel.low,
+          message:'email was not sent',
+          origin:'email.service.ts',
+
         });
         console.log(sentInformation)
       return true;
     } catch (error) {
       return false;
     }
+  }
+
+  async sendEmailWithFileSystemLogs(to:string | string[]){
+    const subject= 'Logs del servidor';
+    const htmlbody= `
+      <h3>Logs de sistemas</h3>
+      <p>mi prueba</p>
+      <p>bueno lo que venga</p>
+      `;
+    const attachement:Attachement[]=[
+        {filename:'logs-high.log',path:'./logs/logs-high.log'},
+        {filename:'logs-low.log',path:'./logs/logs-low.log'},
+        {filename:'logs-medium.log',path:'./logs/logs-medium.log'}
+    ]
+
+   return this.sendEmail({
+      to,subject,htmlbody,attachement
+    })
+
   }
 }
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,34 +1,27 @@
 //
 
-import { CheckService } from "../domain/use-cases/checks/cehcks-service";
+
 import { LogRepositoryImpl } from "../infrastructure/log.respository.impl";
-import { CronService } from "./cron/cron.service";
 import { FileSystemDatasource } from '../infrastructure/datasources/file-system.datasource';
-import { envs } from "../config/plugins/envs.plugin";
 import { EmailService } from "./email/email.service";
+import { SendEmailLogs } from "../domain/use-cases/email/send-logs";
 
 const fileSystemLogRepository=new LogRepositoryImpl(
   new FileSystemDatasource()
 );
 
+const emialService= new EmailService();
 export class Server {
-
 
 
 
     public static start(){// le colocamos static para usar el metodo 
         //directamente  y no crear un nueva instancia 
-      console.log('Server started...')
-      const emialService= new EmailService();
-      emialService.sendEmail({
-        to:'cvelascosaavedra@gmail.com'
-        ,subject:'Lgos System',
-        htmlbody:`
-        <h3>Logs de sistemas</h3>
-        <p>mi prueba</p>
-        <p>bueno lo que venga</p>
-        `
-      })
+      // console.log('Server started...')
+      // new SendEmailLogs(emialService,fileSystemLogRepository).execute(  ['cvelascosaavedra@gmail.com'])
+      // emialService.sendEmailWithFileSystemLogs(
+      //   ['cvelascosaavedra@gmail.com']
+      // )
 
       //  job.start();
       //MANDAR EMAIL
